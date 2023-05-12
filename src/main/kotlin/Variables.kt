d381ee7880e576d3e28227f0033c9e35d8565bba
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-05-12 10:29:14 +0300

    Initio commit

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,20 @@
+
+
+fun main(args: Array<String>){
+    println();
+    println("Hello programmer, congrats you just wrote your first App with Kotlin");
+    println("Please don't stop at learning Kotlin");
+    println("Wait for you on next lesson!");
+
+    val test1 = "Hello"
+    println(test1)
+
+    var test2 = "Hello"
+    test2 = "Goodbye"
+
+    println(test2)
+
+    println("value of test2 $test1")
+
+    println("Sum is ${2 + 2}")
+}
